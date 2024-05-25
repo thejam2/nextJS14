a4328941d51479193805e16a736d9fa1d949e823
thejam2 <thejam2@naver.com>
2024-05-25 18:48:04 +0900

    Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,22 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    const response = fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await (await response).json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
